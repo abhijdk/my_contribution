cd65a8de6624f476cd0541ae558c120e18a38706
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-05-31 15:31:36 +0530

    Perform the multiplication of two given matrix
    Write a program to perform the multiplication of two given matrix and print the data

---
+++ Others/Array_2D4.java
@@ -0,0 +1,59 @@
+//Program: 4
+package mar_13_2023;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array_2D4 
+{	
+	public static void main(String [] args)
+	{//1st 2d arr
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Row length");
+		int row =sc.nextInt();
+		System.out.print("Enter Column length");
+		int col=sc.nextInt();
+		int [][] arr=new int [row][col] ;
+		for (int i=0;i<arr.length;i++)
+		{
+			for (int j=0;j<arr.length;j++)
+			{
+				System.out.print("1st Elements["+i+"]["+j+"]=");
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		//2nd 2d arr
+		int [][] arr2=new int [row][col] ;
+		for (int i=0;i<arr2.length;i++)
+		{
+			for (int j=0;j<arr2.length;j++)
+			{
+				System.out.print("2nd arr Elements["+i+"]["+j+"]=");
+				arr2[i][j]=sc.nextInt();
+			}
+		}
+		//addition
+		int [][] arrMul=new int [row][col] ;
+		for (int i=0;i<arr2.length;i++)
+		{
+			for (int j=0;j<arr2.length;j++)
+			{
+				arrMul[i][j]=0;
+				for (int k=0;k<arr2.length;k++)
+				{
+					arrMul[i][j]=arrMul[i][j]+arr[i][k]*arr2[k][j];
+				}
+			}
+		}
+		System.out.println("1st arr");
+		for (int[] x:arr)//1st arr
+			System.out.println(Arrays.toString(x));
+		System.out.println("2nd arr");
+		for (int[] x:arr2)//2nd arr
+			System.out.println(Arrays.toString(x));
+		System.out.println("Multiplication of both arr");
+		for (int[] x:arrMul)//addition of both arr
+			System.out.println(Arrays.toString(x));
+	}
+}

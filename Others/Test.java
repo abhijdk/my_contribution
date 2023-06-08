5e9bee52033e8461d3c1ef08290e3de95db7113e
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-06-08 17:05:51 +0530

    simple program with FileReader
    We have a class named as Test
    Instance Variables:
    file: File : static
    content: String : static
    Methods:
    It contains main method to test the class for solutions using try and catch to
    catch the exception.
    For try
    File must be read by using BufferReader and FileReader class, read the file
    data, while reading file is not null print the content on the file after close
    BufferReader.
    For catch
    We have three catch blocks for handling the exception
    If file already exits it catches FileAlreadyExistsException and print the
    message
    If file not available in system so it catches FileNotFoundException and print
    the message

--- Others/Test.java
+++ Others/Test.java
@@ -1,88 +1,90 @@
-package may_25_2023;
+package apr_12_2023_IOStream_with_exception_handling;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+
+public class Test 
+{
+	static File file = null;
+	static String content; 
+	public static void main(String[] args) throws IOException
+	{
+		try
+		{
+			file=new File("D:\\Test\\Sir1.txt");
+			BufferedReader br=new BufferedReader(new FileReader(file));
+			if(file.exists())
+			{
+				
+				while ((content=br.readLine())!=null)
+					System.out.println(content);	
+				
+				throw new FileAlreadyExistsException(null);
+			}
+			else
+			{
+				throw new FileNotFoundException("not");
+			}
+			
+		}
+		catch(FileAlreadyExistsException e)
+		{
+			System.err.println("File is already avalable*");
+		}
+		catch(FileNotFoundException e)
+		{
+			System.err.println("File is Not Avalable**");
+		}
+		catch(Exception e)
+		{
+			System.out.println("***");
+			e.printStackTrace();
+		}
+	}
+}/**/
 
 /*
- We have two classes named as FindStudent and Test
-For FindStudent class
-Instance Variables:
-name: String
-id: int
-Methods:
-Two parameterized constructor
-FindStudent(String, int) : initialize instance variables
-getName(): String
-getId() : int
-toString() : @Override
-For class Test
-This class contains main method to test all the solutions for classes and methods and add() method
-for main method
-Local variable:
-studenList : List< FindStudent >
-result: List<FindStudent>
-print result
-for add() method
-add(List, Predicate): List< FindStudent >: public: static
-Local variable:
-newList :  List< FindStudent >
-By using for each if pradicate.test of student so, add the elements to newList then return newList
-*/
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
-
-class FindStudent {
-    private String name;
-    private int id;
-
-    public FindStudent(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return "Name: " + name + ", ID: " + id;
-    }
-}
-
-public class Test {
-    public static void main(String[] args) {
-        List<FindStudent> studentList = createStudentList();
-        List<FindStudent> result = add(studentList, student -> student.getId() > 100);
-        printResult(result);
-    }
-
-    public static List<FindStudent> add(List<FindStudent> studentList, Predicate<FindStudent> predicate) {
-        List<FindStudent> newList = new ArrayList<>();
-        for (FindStudent student : studentList) {
-            if (predicate.test(student)) {
-                newList.add(student);
-            }
-        }
-        return newList;
-    }
-
-    private static List<FindStudent> createStudentList() {
-        List<FindStudent> studentList = new ArrayList<>();
-        studentList.add(new FindStudent("Jaga", 101));
-        studentList.add(new FindStudent("Kalia", 102));
-        studentList.add(new FindStudent("Depak", 103));
-        studentList.add(new FindStudent("Manoj", 104));
-        studentList.add(new FindStudent("Manas", 105));
-        return studentList;
-    }
-
-    private static void printResult(List<FindStudent> result) {
-        for (FindStudent student : result) {
-            System.out.println(student);
-        }
-    }
-}
+public class Test 
+{
+	static File file = null;
+	static String content; 
+	public static void main(String[] args) throws IOException
+	{
+		try
+		{
+			file=new File("D:\\Test\\Sir1.txt");
+			BufferedReader br=new BufferedReader(new FileReader(file));
+			if(file.exists())
+			{
+				
+				while ((content=br.readLine())!=null)
+					System.out.println(content);	
+				
+				throw new FileAlreadyExistsExceptionDemo("File is Existing");
+			}
+			else
+			{
+				throw new FileNotFoundExceptionDemo("not found");
+			}
+			
+		}
+		catch(FileAlreadyExistsExceptionDemo e)
+		{
+			System.err.println("File is already avalable*");
+		}
+		catch(FileNotFoundExceptionDemo e)
+		{
+			System.out.println(e.getMessage());
+		}
+		catch(Exception e)
+		{
+			System.out.println("***");
+			e.printStackTrace();
+		}
+	}
+}/**/

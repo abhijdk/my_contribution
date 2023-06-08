2bb9801d4c4fab49d4484d5b7a4fa2fec039709b
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-06-08 17:00:57 +0530

    CharacterTest with Exception
    Write a program to create user-defined checked exception which will accept only vowels character from the user, if the character is not vowel then it should throw InvalidCharacerException with proper message

---
+++ Others/CharacterTest.java
@@ -0,0 +1,21 @@
+package apr_10_2023_user_defined_un_checked_exception;
+
+import java.util.Scanner;
+
+//public class CharacterTest
+//{
+//	public static void main(String[] args) 
+//	{
+//		Scanner sc=new Scanner(System.in);
+//		System.out.print("Entr a character: ");
+//		try(sc)
+//		{
+//			char c=CharacterLogic.character(sc.next().charAt(0));
+//			System.out.println(c+" is a vowel character");
+//		}
+//		catch(InvalidCharacterException e)
+//		{
+//			System.out.println("Error*** "+e.getMessage());
+//		}
+//	}
+//}

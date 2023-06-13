89cf3e5135856880151b6faf0aa71c8b4e3662be
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-06-13 16:41:30 +0530

    HarshadNumber
    GCD and LCM are two commonly used terms in mathematics, specifically when dealing with multiple numbers. 
    
    GCD stands for "Greatest Common Divisor," also known as the highest common factor. It refers to the largest positive integer that divides two or more numbers without leaving a remainder. In other words, it is the largest number that divides all the given numbers evenly. The GCD is often used to simplify fractions or find common factors between numbers.
    
    LCM stands for "Least Common Multiple." It refers to the smallest positive integer that is divisible by two or more numbers without leaving a remainder. In other words, it is the smallest number that is a multiple of all the given numbers. The LCM is often used to find a common multiple between numbers or to solve problems involving periodic events.
    
    Both GCD and LCM are important concepts in number theory and have various applications in mathematics, computer science, and real-life problem-solving. They are used to analyze and manipulate numbers, simplify calculations, and solve problems involving multiple quantities.

---
+++ DSA/HarshadNumber.java
@@ -0,0 +1,24 @@
+package june.month;
+
+import java.util.Scanner;
+
+public class HarshadNumber {
+	public static void main(String[] args) {
+		System.out.print("Enter A Number");
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int i=num;
+		int sum=0;
+		while(i!=0) {
+			int j=(i%10);
+			sum+=j;
+			i=i/10;
+		}
+		
+		if(num%sum==0) {
+			System.out.println(num+" is a Harshad Number");
+		}else {
+			System.out.println(num+" is not a Harshad Number");
+		}
+	}
+}

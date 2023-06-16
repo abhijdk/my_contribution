9313bc897be489bdc5b5c37ab8114b1be860ff54
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-06-16 09:46:24 +0530

    T_Type_Parameter
    //<T> type parameter :
    //---------------------
    //-> It is known as Type parameter.
    //
    //-> It is independent type which will accept all the Warpper     
    //   classes and userdefined class

--- Others/T_Type_Parameter.java
+++ Others/T_Type_Parameter.java
@@ -1,4 +1,4 @@
-
+package may_23_2023;
 
 //<T> type parameter :
 //---------------------
@@ -9,30 +9,24 @@
 //
 //Program :
 //---------
-class Test<T> {
+class Test<T>{
 	T obj;
-
-	public void setObj(T obj) //setter
-	{
-		this.obj = obj;
+	public void setObj(T obj) {
+		this.obj=obj;
 	}
-
-	public T getObj() //getter
-	{
+	public T getObj() {
 		return this.obj;
 	}
 }
 
 public class T_Type_Parameter {
-	public static void main(String [] args)
-	{
-       Test<Integer> ti = new Test<Integer>();
-	   ti.setObj(353);
-	   System.out.println(ti.getObj());
-
-	   Test<String> ts = new Test<String>();
-	   ts.setObj("OMM");
-	   System.out.println(ts.getObj());
-      
+	public static void main(String[] args) {
+		Test <Integer> t=new Test<Integer> ();
+		t.setObj(353);
+		System.out.println(t.getObj());
+		
+		Test<String> s=new Test();
+		s.setObj("OMM");
+		System.out.println(s.getObj());
 	}
 }

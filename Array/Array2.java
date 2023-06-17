1d790daa7e76599f9499500b8fe1df9250ee0062
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-06-17 23:21:04 +0530

    Array2
    //Given an array of integers, return true if the array length is 1 or more,and the first element and the last elements both are equal.

---
+++ Array/Array2.java
@@ -0,0 +1,18 @@
+public class ArrayDemo2 
+{
+	public static void main(String[] args)
+	{
+		int []x = {12,12,90,89,56,12};
+		boolean b = sameFirstLast(x);
+		System.out.println(b);
+	}
+
+	public static boolean sameFirstLast(int[] nums)
+	{
+		if (nums.length >= 1 && nums[0] == nums[nums.length-1]) 
+			return true;
+		else
+		   return false;
+	}
+
+}

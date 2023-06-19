99610abf8e035c23d513c06936fc21e93349a5a5
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-06-19 17:33:47 +0530

    Create ArrayDefault.java
    Whenever we create an array object using new keyword, all the array index will be initialized with default values as shown in the program below.

---
+++ Array/ArrayDefault.java
@@ -0,0 +1,12 @@
+package com.abhi.communication;
+
+public class ArrayDefault {
+
+	public static void main(String[] args){
+		String []x = new String[10];
+		
+		for(String y : x){
+			System.out.println(y);
+		}
+	}
+}

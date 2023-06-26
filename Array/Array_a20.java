5f6e314c5985b7098748cf18572fbf505ca1363b
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-06-26 22:08:02 +0530

    Create Array_a20.java
    Write a Java program to convert an array to an ArrayList

---
+++ Array/Array_a20.java
@@ -0,0 +1,11 @@
+package may_exm1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Array_a20 {
+	public static void main(String[] args) {
+		String arr[]= {"AAA","BBB","CCC","DDD","EEE"};
+		ArrayList<String> al=new ArrayList<String>(Arrays.asList(arr));
+		System.out.println(al);
+	}
+}

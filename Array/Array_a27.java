391af2f99573703300e5e28f541cd5d7e76e9550
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-06-28 12:33:22 +0530

    Array_a27.java
    Write a Java program to get the difference between the largest and smallest values in an array of integers. The array must have a length of at least 1

--- Array/Array_a27.java
+++ Array/Array_a27.java
@@ -4,16 +4,23 @@ package may_exm1;
 public class Array_a27 {
 	public static void main(String[] args) {
 		int arr[]= {14,2,6,41,7,5,8};
-		int odd=0;
-		int eve=0;
-		for(int i=0;i<arr.length;i++) {
-			if(arr[i]%2==0) {
-				eve++;
-			} else {
-				odd++;
+		
+		int small=0;
+		int large=0;
+		
+		
+		for(int i=1;i<arr.length;i++) {
+			if(arr[small]>arr[i]) {
+				arr[small]=arr[i];
 			}
 		}
-		System.out.println("No. of odd number is: "+odd);
-		System.out.println("No. of even number is: "+eve);
+		small=arr[small];
+		for(int i=1;i<arr.length;i++) {
+			if(arr[large]<arr[i]) {
+				arr[large]=arr[i];
+			}
+		}
+		
+		System.out.println("Difference between the largest and smallest values of the said array: "+(arr[large]-small));
 	}
 }

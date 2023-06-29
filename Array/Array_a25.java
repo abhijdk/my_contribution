007382cbcac9e8780747791b32d8ffae9b977b08
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-06-29 10:25:57 +0530

    Create Array_a25.java
    Write a Java program to find common elements in three sorted (in non-decreasing order) arrays.

---
+++ Array/Array_a25.java
@@ -0,0 +1,28 @@
+package may_exm1;
+
+
+import java.util.*;
+public class Array_a25 {
+public static void main(String[] args) {
+	ArrayList<Integer> common = new ArrayList<Integer>();
+   int array1[] = {2, 4, 8};
+   int array2[] = {2, 3, 4, 8, 10, 16};
+   int array3[] = {4, 8, 14, 40};
+	int x = 0, y = 0, z = 0;
+	while (x < array1.length && y < array2.length && z < array3.length){
+		if (array1[x] == array2[y] && array2[y] == array3[z]){
+			common.add(array1[x]);
+			x++;
+			y++;
+			z++;
+		}
+		else if (array1[x] < array2[y])
+			x++;
+		else if (array2[y] < array3[z])
+			y++;
+		else
+			z++;
+	}
+	System.out.println("Common elements from three sorted (in non-decreasing order ) arrays: "+common);
+}
+}

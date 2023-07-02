6e9c7ca3ca08c40e13a40f2d0ba85fb655b0bcb8
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-07-02 20:37:02 +0530

    Create Array_a42.java
    Write a Java program to separate 0s and 1s in an array of 0s and 1s into left and right sides.

---
+++ Array/Array_a42.java
@@ -0,0 +1,14 @@
+package may_exm1;
+
+import java.util.Arrays;
+
+public class Array_a42{
+   public static void main (String[] args) {
+	   int arr[]= {0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,0};
+	   
+	   Arrays.sort(arr);
+	   
+	   
+	   System.out.println("Array after segregation is : "+Arrays.toString(arr));
+   }
+}

dc81abab1fbcfe1a65e8feccc6f034e6c7a2aa10
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-07-03 13:00:18 +0530

    Create Array_a47.java
    Write a Java program to arrange the elements of an array of integers so that all positive integers appear before all negative integers.

---
+++ Array/Array_a47.java
@@ -0,0 +1,32 @@
+package may_exm1;
+
+import java.util.Arrays;
+
+public class Array_a47{
+   public static void main (String[] args) {
+	   int arr[]= {-10,10,-20,20,-30,30,-40,40};
+	   int arr2[]=new int[arr.length];
+	   int c=0;
+	   
+	   for(int i=0;i<arr.length;i++) {
+		   if(arr[i]>=-1) {
+			   arr2[c]=arr[i];
+			   c++;
+		   }
+	   }
+	   
+	   for(int i=0;i<arr.length;i++) {
+		   if(arr[i]<=-1) {
+			   arr2[c]=arr[i];
+			   c++;
+		   }
+	   }
+	   System.out.println("Old Array: "+Arrays.toString(arr));
+	   System.out.println("New Array: "+Arrays.toString(arr2));
+   }
+}
+
+/* Out Put
+Old Array: [-10, 10, -20, 20, -30, 30, -40, 40]
+New Array: [10, 20, 30, 40, -10, -20, -30, -40]
+/**/

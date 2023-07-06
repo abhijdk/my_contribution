d08c9a2951ee6eabf1641d5147ff15227616ce74
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-07-07 01:18:33 +0530

    Create Square.java
    find the Square of below element
    0+1+x+x2+x3+x4+......................
    (here x2 means Square of x)

---
+++ Others/Square.java
@@ -0,0 +1,21 @@
+package may_exm1;
+
+
+
+public class Square
+{
+	public static void main(String [] ab) {
+		int sum=0; 
+		for(int i=0;i<10;i++) {
+			sum+=Math.pow(5, i);
+		}
+		System.out.println(sum);
+	}
+}
+
+
+
+
+/* Out Put
+2441406
+*/

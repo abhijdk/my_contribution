4f637c04e4f763a900c358ae87d41e5a7f0d57df
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-07-12 23:17:45 +0530

    Create MyReplace.java
    Write a Java program to replace a specified character with another character

---
+++ String/MyReplace.java
@@ -0,0 +1,13 @@
+package Demo;
+
+public class MyReplace {
+    
+	   public static void main(String[] args)
+	    {
+	        String s="Java is a Language";
+	        String myReplace=s.replace('a', 'A');
+	        
+	        System.out.println("Old String: "+s);
+	        System.out.println("New String: "+myReplace);
+	    }
+	}

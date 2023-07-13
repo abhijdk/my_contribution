a292a3ce86c2c8813e988f02bb1db24561e38693
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-07-13 14:50:47 +0530

    Create MySubstring.java
    Write a Java program to get a substring of a given string at two specified positions

---
+++ String/MySubstring.java
@@ -0,0 +1,15 @@
+package Demo;
+
+public class MySubstring {
+    
+	   public static void main(String[] args)
+	    {
+	        String s1="Java was developed by James Gosling";
+	        String s2="Java was introduced in the year of 1995";
+	        String s3="Python was developed by Guido van Rossum";
+	        
+	        System.out.println(s1.substring(22,35));
+	        System.out.println(s2.substring(35,39));
+	        System.out.println(s3.substring(24,40));
+	    }
+}

c2a1e4907b88eb0e170a3da34aa5c4e373939b70
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-07-14 17:41:43 +0530

    Create MyLowerCash.java
    Write a Java program to convert all characters in a string to lowercase.

---
+++ String/MyLowerCash.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class MyLowerCash {
+    
+	   public static void main(String[] args){
+	        String s1="Java was developed by James Gosling";
+	        String s2=s1.toLowerCase();
+	        System.out.println(s2);
+	    }
+}

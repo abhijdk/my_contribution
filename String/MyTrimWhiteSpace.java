9b739492d4912335dc98318a04f7d22a73bf7ecb
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-07-14 17:44:08 +0530

    Create MyTrimWhiteSpace.java
    Write a Java program to trim leading or trailing whitespace from a given string

---
+++ String/MyTrimWhiteSpace.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class MyTrimWhiteSpace {
+    
+	   public static void main(String[] args){
+	        String s1="                  Java was developed by James Gosling                  ";
+	        String s2=s1.trim();
+	        System.out.println(s2);
+	    }
+}

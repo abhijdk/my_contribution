a846b4554b07a22ac8335bf9c3f730e61457ec78
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-07-14 17:39:12 +0530

    Create StringToCharArr.java
    Write a Java program to create a character array containing a string.

---
+++ String/StringToCharArr.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class StringToCharArr {
+    
+	   public static void main(String[] args){
+	        String s1="Java was developed by James Gosling";
+	        char []arr=s1.toCharArray();
+	        System.out.println(arr);
+	    }
+}

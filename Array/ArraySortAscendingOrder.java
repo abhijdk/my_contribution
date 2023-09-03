43fac2293927d75eaa7682e4b46a8c07ac898603
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-09-03 21:46:46 +0530

    Create ArraySortAscendingOrder.java
    Sort the array with ascending Order

---
+++ Array/ArraySortAscendingOrder.java
@@ -0,0 +1,11 @@
+package hacker_rank_codes;
+
+import java.util.Arrays;
+
+public class Main{
+	public static void main(String ...strin) {
+		int [] arr= {0,2,4,5,3,1};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+}

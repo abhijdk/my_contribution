6781b1bd1cccbc3b7e4048792cae31ef13cd591e
Abhinna Sundar Behuria <127319599+abhijdk@users.noreply.github.com>
2023-09-03 21:55:29 +0530

    Create StringPalindrome.java
    String Palindrome using predefined method

---
+++ String/StringPalindrome.java
@@ -0,0 +1,10 @@
+package hacker_rank_codes;
+
+
+public class Main{
+	public static void main(String ...string) {
+		String str="racecar";
+		String newStr=new StringBuilder(str).reverse().toString();
+		System.out.println(newStr);
+	}
+}
